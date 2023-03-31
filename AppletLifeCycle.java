6b7922a0ca7ccdedd5b6dbc200af18d142881a8d
ANIDIP LAHA <107384020+anidiplaha@users.noreply.github.com>
2023-03-31 12:12:36 +0530

    Add files via upload

---
+++ AppletLifeCycle.java
@@ -0,0 +1,29 @@
+import java.applet.Applet; 
+ import java.awt.Graphics; 
+ import java.awt.*; 
+ 
+ public class AppletLifeCycle extends Applet 
+ { 
+      public void init() 
+      { 
+        setBackground(Color.CYAN); 
+        System.out.println("init() called"); 
+      } 
+      public void start()
+	{ 
+		System.out.println("Start() called"); 
+	} 
+      public void paint(Graphics g)
+	{ 
+		System.out.println("Paint(() called"); 
+	} 
+      public void stop() 
+	{ 
+		System.out.println("Stop() Called"); 
+	} 
+      public void destroy()   
+	{ 
+		System.out.println("Destroy)() Called"); 
+	} 
+ } 
+ 
